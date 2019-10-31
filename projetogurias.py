9ade381b14cee267365fe5e17131d59d1f9a560a
luanlelis12 <luanlelis09@gmail.com>
2019-10-31 16:57:07 -0300

    add na rota cadastro o formulario

--- projetogurias.py
+++ projetogurias.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from forms import CadastraUsuarioForm
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projetoguriais.db'
 app.config['SECRET_KEY'] = 'd77a98b1c04bc37e50787352e80323e3'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    passaword = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/index')
 def index():
@@ -9,14 +23,19 @@ def index():
 @app.route('/login')
 def login():
     return render_template('pages/login.html')
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['GET', 'Post'])
 def cadastro():
-    return render_template('pages/cadastro.html')
+
+    form= CadastraUsuarioForm()
+
+    return render_template('pages/cadastro.html', formulario=form)
 @app.route('/lute')
 def lute():
     return render_template('pages/lute.html')
 @app.route('/acompanhamento')
 def acompanhamento():
     return render_template('pages/acompanhamento.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
