ba3e966f4c9aacc248ef7083b8794c88974238c9
dominiquetrindade <mick22009@hotmail.com>
2019-11-07 16:35:43 -0300

    add tabela visitante

--- projetogurias.py
+++ projetogurias.py
@@ -15,6 +15,14 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     datadenascimento = db.Column(db.Date(), nullable =False)
 
+class Visitante(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    datadenascimento = db.Column(db.Date(), nullable =False)
+    escrevaRelato = db.Column(db.String(1000))
+
+
 
     def __repr__(self):
         return '<User %r>' % self.username
